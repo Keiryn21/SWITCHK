c6f47133d3592379ecc4139030abcb690c4dc7ff
Keiryn21 <00061225@uca.edu.sv>
2025-06-04 14:35:49 -0600

    dias

---
+++ 1.cpp
@@ -0,0 +1,50 @@
+#include <iostream>
+using namespace std;
+int main (){
+
+    int dia;
+
+    cout<<"por favor elige una de las siguientes opciones"<<endl;
+    cout<<"1"<<endl;
+    cout<<"2"<<endl;
+    cout<<"3"<<endl;
+    cout<<"4"<<endl;
+    cout<<"5"<<endl;
+    cout<<"6"<<endl;
+    cout<<"7"<<endl;
+    cin>>dia;
+
+
+    switch (dia){
+
+    case 1:
+    cout<<"lunes"<<endl;
+    break;
+
+    case 2:
+    cout<<"martes"<<endl;
+    break;
+
+    case 3:
+    cout<<"miercoles"<<endl;
+    break;
+
+    case 4:
+    cout<<"jueves"<<endl;
+    break;
+
+    case 5:
+    cout<<"viernes"<<endl;
+    break;
+
+    case 6:
+    cout<<"sabado"<<endl;
+    break;
+    default:
+
+    case 7:
+    cout<<"domingo"<<endl;
+    break;}
+
+    return 0;
+}
